2649d6814e48983d4254dfcbf2f6545322137664
somethingkindawierd <somethingkindawierd@gmail.com>
2010-09-27 22:04:51 -0700

    Some work or Rectangle...still have long way to go.

--- test/rectangle.test.js
+++ test/rectangle.test.js
@@ -2,6 +2,16 @@ var r = new Rectangle(0, 0, 10, 20);
 
 console.log('Rectangle:', r.toString());
 
-r.setWidth(20);
+r.width = 20;
 
 console.log('Rectangle:', r.toString());
+
+console.log('equals ? ', r.equals(new Rectangle(0, 0, 10, 20)), ' [expect false]');
+console.log('equals ? ', r.equals(new Rectangle(0, 0, 20, 20)), ' [expect true]');
+
+var  inside = new Rectangle(1, 1, 18, 18);
+
+console.log(r.toString());
+console.log(inside.toString());
+
+console.log('inside ? ', r.containsRect(inside)), '[expect true]';
