7ca6b86a94f667c5dd438d62e606814a53b395ab
somethingkindawierd <somethingkindawierd@gmail.com>
2010-10-01 19:01:08 -0700

    Added some more inheritance goodness. Reworked the Sprite class, and added the Shape class, with proper inheritance heirarchy. We are ready to start fleshing out the classes and get some stuff drawn onto the canvas :)

---
+++ test/flash/display/displayObject.test.js
@@ -0,0 +1,31 @@
+
+
+console.log('----------------------------------------------');
+
+
+var shape = new Shape();
+console.log('Created: ', shape.toString(), shape);
+
+shape.x = 50;
+shape.y = 90;
+shape.width = 160;
+shape.height = 100;
+shape.alpha = 0.5;
+
+console.log('Created: ', shape.toString(), shape);
+
+
+console.log('----------------------------------------------');
+
+
+var sprite = new Sprite();
+console.log('Created: ', sprite.toString(), sprite);
+
+sprite.x = 50;
+sprite.y = 90;
+
+console.log('Created: ', sprite.toString(), sprite);
+
+
+console.log('----------------------------------------------');
+
