68329df8db9f1e5213ac0c07584f7bced2cf22ef
somethingkindawierd <somethingkindawierd@gmail.com>
2010-10-27 22:54:22 -0700

    Stage work -- added basic event dispatching and tests for adding/swapping items in display list

--- test/flash/display/Stage.test.js
+++ test/flash/display/Stage.test.js
@@ -4,14 +4,13 @@
 // when the flash plugin is created.
 
 window.onload = function() {
-  console.log('window.onload');
-  //setTimeout(init, 500);
+  trace('window.onload');
   init();
 };
 
 function init() {
 
-  console.log('init');
+  trace('init');
 
 /*
   var elem = document.getElementById('stageCanvas');
@@ -33,16 +32,25 @@ function init() {
   }
   */
 
+  // Let's listen for some events
+  var listener = {
+
+    onAdded: function(event) {
+      trace('*** Caught Event*** Stage.added: ', event);
+    }
+
+  };
+
   var S = new Stage('stageCanvas');
-  console.log('Stage object: ', S);
+  trace('Stage object: ', S);
 
-  console.log('Stage.width = ' + S.width);
+  trace('Stage.width = ' + S.width);
   S.width = 500;
-  console.log('Stage.width = ' + S.width);
+  trace('Stage.width = ' + S.width);
 
-  console.log('Stage.height = ' + S.height);
+  trace('Stage.height = ' + S.height);
   S.height = 200;
-  console.log('Stage.height = ' + S.height);
+  trace('Stage.height = ' + S.height);
 
   S.align = StageAlign.TOP_LEFT;
   S.displayState = StageDisplayState.NORMAL;
@@ -51,7 +59,62 @@ function init() {
   S.scaleMode = StageScaleMode.NO_SCALE;
   S.showDefaultContextMenu = false;
 
-  console.log('Stage object: ', S);
+  S.addEventlistener(Event.ADDED, listener.onAdded);
 
+  trace('Stage object: ', S);
+
+  trace('----------------------------------------------');
+
+  var shape = new Shape();
+  trace('Created: ', shape.toString(), shape);
+
+  shape.x = 50;
+  shape.y = 90;
+  shape.width = 160;
+  shape.height = 100;
+  shape.alpha = 0.5;
+  shape.name = 'Ken';
+
+  trace('Created: ', shape.toString(), shape);
+
+  S.addChild(shape);
+
+  trace('----------------------------------------------');
+
+  var shape2 = new Shape();
+  trace('Created: ', shape2.toString(), shape2);
+
+  shape2.x = 10;
+  shape2.y = 10;
+  shape2.width = 120;
+  shape2.height = 80;
+  shape2.alpha = 0.8;
+  shape2.name = 'Barbie';
+
+  trace('Created: ', shape2.toString(), shape2);
+
+  S.addChild(shape2);
+
+  trace('----------------------------------------------');
+
+  trace('Getting Shape "Ken" by name: ', S.getChildByName('Ken'));
+
+  trace('Getting Shape "Barbie" by name: ', S.getChildByName('Barbie'));
+
+  trace('----------------------------------------------');
+
+  trace('Will swap children...');
+
+  trace('Display List before: ', S.displayList_);
+
+  S.swapChildren(S.getChildByName('Ken'), S.getChildByName('Barbie'));
+
+  trace(S.displayList_);
+
+  S.swapChildrenAt(0,1);
+
+  trace(S.displayList_);
+
+  trace('----------------------------------------------');
 
 }
