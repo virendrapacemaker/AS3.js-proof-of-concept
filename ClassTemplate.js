7c2cd3841b8393fda943a54ad69fc6de55ec0826
somethingkindawierd <somethingkindawierd@gmail.com>
2011-02-05 21:48:42 -0800

    Cleaned up Class Template (experimenting with defining classes in closures,) reworked beebe.func + added unit tests.

--- ClassTemplate.js
+++ ClassTemplate.js
@@ -1,108 +1,111 @@
-goog.provide('flash.ns.Class');
 goog.provide('Class');
+goog.provide('flash.ns.Class');
 
 goog.requires('flash.ns');
 
-/**
- * Class Template
- *
- * @constructor
- */
-flash.ns.Class = function() {
+(function() {
+
+  /**
+   * Class Template
+   *
+   * @constructor
+   */
+  flash.ns.Class = function() {
+
+    //--------------------------------------------------------------------------
+    // < Private Attributes >                                                  -
+    // Not truly private, but the _ denotes it                                 -
+    // Google Closure Compiler will treat as private and offer compile errors  -
+    // if used incorrectly. Note Google Closure Styles recommend private       -
+    // properties end with a trailing _ (underscore).                          -
+    //--------------------------------------------------------------------------
+
+    /** @private */
+    this.name_;
+
+    /** @private */
+    this.other_;
+
+    /** @private */
+    this.other2_;
+
+    //--------------------------------------------------------------------------
+    // </ Private Attributes >                                                 -
+    //--------------------------------------------------------------------------
+
+  };
+  goog.inherits(flash.ns.Class, Parent);
+  // Notice no new-line between this & constructor
+  // Class -> Parent Class -> Parent Class -> Object
 
   //----------------------------------------------------------------------------
-  // < Private Attributes >                                                    -
-  // Not truly private, but the _ denotes it                                   -
-  // Google Closure Compiler will treat as private and offer compile errors    -
-  // if used incorrectly                                                       -
+  //< Getters n Setters >                                                      -
+  // Here we follow the standard javascript get/set definition pattern of      -
+  // getName/setName. Using the magic method seen at the bottom of the script  -
+  // these functions will be used to define official getters/setters.          -
+  //----------------------------------------------------------------------------
+
+  flash.ns.Class.prototype.getName = function() {
+    return this.name_;
+  };
+
+  flash.ns.Class.prototype.setName = function(val) {
+    this.name_ = val;
+  };
+
+  //----------------------------------------------------------------------------
+  // These are not supported by ECMA 3, and thus not by Google Closure         -
+  // compiler. Someday...                                                      -
+  //----------------------------------------------------------------------------
+
+  //Object.defineProperties(Class.prototype, {
+  //  name: {
+  //    get: function() {
+  //      return this.name_;
+  //    },
+  //    set: function(val) {
+  //      this.name_ = val;
+  //    }
+  //  }
+  //});
+
+  //----------------------------------------------------------------------------
+  // </ Getters n Setters >                                                    -
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  // < Public Methods >                                                        -
   //----------------------------------------------------------------------------
 
   /**
-   * @private
+   * Some public method.
+   * @param {type} param Description.
    */
-  this.name_;
+  flash.ns.Class.prototype.publicMethod = function(param) {
+
+    // TODO: function body
+
+  };
 
   /**
-   * @private
+   * Render object to string.
+   * @override
+   * @return {string} Object as string.
    */
-  this.other_;
+  flash.ns.Class.prototype.toString = function toString() {
 
-  /**
-   * @private
-   */
-  this.other2_;
+    return this.name_;
 
-  //----------------------------------------------------------------------------
-  // </ Private Attributes >                                                   -
-  //----------------------------------------------------------------------------
+  };
 
-};
-goog.inherits(flash.ns.Class, Parent); // Notice no new-line between this & constructor
-// Class -> Parent Class -> Parent Class -> Object
+  // Here we automagically turn all getName and setName methods into official
+  // ECMA 5 getters and setters. We don't do this above because it's not
+  // supported by Google Closure.
+  beebe.magic.enhanceWithGettersAndSetters2(flash.ns.Class.prototype);
 
-//------------------------------------------------------------------------------
-//< Getters n Setters >                                                        -
-// These are set using the magic enhance method seen at bottom of script       -
-//------------------------------------------------------------------------------
+  // Export symbols to global namespace. Prevents Google from renaming them.
+  goog.exportSymbol('flash.ns.Class', flash.ns.Class);
+  goog.exportSymbol('Class', flash.ns.Class);
 
-flash.ns.Class.prototype.getName = function() {
-  return this.name_;
-};
+})();
 
-flash.ns.Class.prototype.setName = function(val) {
-  this.name_ = val;
-};
-
-//------------------------------------------------------------------------------
-// These are not supported by ECMA 3, and thus not by Google Closure           -
-// compiler. Someday...                                                        -
-//------------------------------------------------------------------------------
-
-//Object.defineProperties(Class.prototype, {
-//  name: {
-//    get: function() {
-//      return this.name_;
-//    },
-//    set: function(val) {
-//      this.name_ = val;
-//    }
-//  }
-//});
-
-//------------------------------------------------------------------------------
-// </ Getters n Setters >                                                      -
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// < Public Methods >                                                          -
-//------------------------------------------------------------------------------
-
-/**
- * Some public method.
- * @param {type} param Description.
- */
-flash.ns.Class.prototype.publicMethod = function(param) {
-
-  // TODO: function body
-
-};
-
-/**
- * Render object to string.
- * @override
- * @return {string} Object as string.
- */
-flash.ns.Class.prototype.toString = function toString() {
-
-  return this.name_;
-
-};
-
-// Here we use a custom sugar method to turn all setBlah and getBlah methods
-// into official ECMA 5 getters and setters. We don't do this above because
-// it's not supported by Google Closure
-_.enhanceWithGettersAndSetters2(flash.ns.Class.prototype);
-
-// Export our symbols to the global namespace. Prevents Google from renaming them.
-goog.exportSymbol('flash.ns.Class', flash.ns.Class);
-goog.exportSymbol('Class', flash.ns.Class);
